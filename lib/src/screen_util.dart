33c2e4d39f5c71f7d7979d18501ca1af4e93a509
HoangLM <>
2023-08-08 14:49:59 +0700

    refactor: add orientation into ScreenUtil.configure() method

--- lib/src/screen_util.dart
+++ lib/src/screen_util.dart
@@ -98,6 +98,7 @@ class ScreenUtil {
     bool? splitScreenMode,
     bool? minTextAdapt,
     FontSizeResolver? fontSizeResolver,
+    Orientation? orientation,
   }) {
     try {
       if (data != null)
@@ -117,7 +118,7 @@ class ScreenUtil {
     final MediaQueryData? deviceData = data.nonEmptySizeOrNull();
     final Size deviceSize = deviceData?.size ?? designSize;
 
-    final orientation = deviceData?.orientation ??
+    orientation ??= deviceData?.orientation ??
         (deviceSize.width > deviceSize.height
             ? Orientation.landscape
             : Orientation.portrait);
